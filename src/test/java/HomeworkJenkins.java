ee95654ef77b6e6f85d99f06c922b137014979cd
irinakohna <irinakokhna@mail.ru>
2021-12-19 19:47:19 +0300

    commit for jenkins

---
+++ src/test/java/HomeworkJenkins.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+import java.io.File;
+
+public class HomeworkJenkins {
+    @Test
+    void homeTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Irina");
+        $("#lastName").setValue("Krbtv");
+        $("#userEmail").setValue("krbtvir@mail.com");
+        $(byText("Female")).click();
+        $("#userNumber").setValue("8005553535");
+        $(byText("Hobbies")).scrollTo();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__day--009:not(react-datepicker__day--009 react-datepicker__day--outside-month)").click();
+        $(By.xpath("//*[@id=\"subjectsInput\"]")).setValue("Maths").pressEnter();
+        $(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/screen/Screenshot 2021-09-02 at 12.26.43.png"));
+        $(By.xpath("//*[@id=\"currentAddress\"]")).setValue("addressless");
+        $(By.xpath("//*[@id=\"state\"]/div/div[2]")).click();
+        $(byText("NCR")).click();
+        $(By.xpath("//*[@id=\"city\"]/div/div[2]")).click();
+        $(byText("Delhi")).click();
+        $(By.xpath("//*[@id=\"submit\"]")).click();
+        $(".table-responsive").shouldHave(text("Irina Krbtv"), text("krbtvir@mail.com"),
+                text ("Screenshot 2021-09-02 at 12.26.43.png"), text("Maths"), text("NCR Delhi"));
+    }
+}
