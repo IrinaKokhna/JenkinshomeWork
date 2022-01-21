8b6dd5eee52d5a2f67b17058a2d74130086da49e
irinakohna <irinakokhna@mail.ru>
2022-01-21 21:59:00 +0300

    added attachments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,6 +4,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.*;
 
 public class TestBase {
         @BeforeAll
@@ -18,4 +19,11 @@ public class TestBase {
             Configuration.startMaximized = true;
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         }
+        @AfterEach
+        public void tearDown() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+    }
 }
